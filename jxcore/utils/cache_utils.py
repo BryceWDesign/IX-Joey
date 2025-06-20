47f734c2b6775c43bc308cdc17d247f6573611fb
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-06-20 14:20:43 -0700

    Add cache_utils.py: reusable LRU cache implementation for knowledge modules

---
+++ jxcore/utils/cache_utils.py
@@ -0,0 +1,29 @@
+# jxcore/utils/cache_utils.py
+
+"""
+Cache Utilities:
+Common caching helpers for various modules.
+"""
+
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, max_size=1000):
+        self.cache = OrderedDict()
+        self.max_size = max_size
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.max_size:
+            self.cache.popitem(last=False)
+
+    def clear(self):
+        self.cache.clear()
