99eb47b5e7047d89dc1b5c1c1f06dd6084c0266f
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-06-20 14:13:13 -0700

    Add latency_cache.py: simple LRU cache for query speed optimization

---
+++ jxcore/knowledge/latency_cache.py
@@ -0,0 +1,30 @@
+# jxcore/knowledge/latency_cache.py
+"""
+Latency Cache:
+- Cache recent query results for fast retrieval
+- Simple LRU eviction policy
+"""
+
+from collections import OrderedDict
+
+class LatencyCache:
+    def __init__(self, max_size=1000):
+        self.cache = OrderedDict()
+        self.max_size = max_size
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.max_size:
+            self.cache.popitem(last=False)
+
+    def clear(self):
+        self.cache.clear()
+
